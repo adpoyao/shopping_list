77949282bf32c526295e0d143322234fb8b0f0a0
Eddie Yao <adpoyao@gmail.com>
2017-09-29 19:36:26 -0700

    finished hide function

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
 /* global $ */
 
-const STORE = [{name: 'apples', checked: false, editable: false, hidden: false},
-  {name: 'oranges', checked: false, editable: false, hidden: false},
-  {name: 'milk', checked: true, editable: false, hidden: false},
-  {name: 'bread', checked: false, editable: false, hidden: false}];
+const STORE = [{name: 'apples', checked: false, editable: false},
+  {name: 'oranges', checked: false, editable: false},
+  {name: 'milk', checked: true, editable: false},
+  {name: 'bread', checked: false, editable: false}];
 
 function generateItemElement(item, itemIndex, template) {
   return `
       <li class="js-item-index-element" data-item-index="${itemIndex}">
-        <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''} ${item.hidden ? 'item__hidden' : ''}" contenteditable='${item.editable}'>${item.name}</span>
+        <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}" contenteditable='${item.editable}'>${item.name}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle js-item-toggle">
               <span class="button-label">check</span>
@@ -26,18 +26,14 @@ function generateItemElement(item, itemIndex, template) {
 
 //<button class="shopping-item-hide js-item-hide">Hide Completed Items</button>
 
-function generateShoppingItemsString(shoppingList) {
-  console.log('Generating shopping list element');
-  
+function generateShoppingItemsString(shoppingList) {  
   const items = shoppingList.map((item, index) => generateItemElement(item, index));
-    
   return items.join('');
 }
   
   
 function renderShoppingList() {
   // render the shopping list in the DOM
-  console.log('`renderShoppingList` ran');
   const shoppingListItemsString = generateShoppingItemsString(STORE);
   
   // insert that HTML into the DOM
@@ -45,7 +41,6 @@ function renderShoppingList() {
 }
   
 function addItemToShoppingList(itemName) {
-  console.log(`Adding "${itemName}" to shopping list`);
   STORE.push({name: itemName, checked: false});
 }
   
@@ -60,7 +55,6 @@ function handleNewItemSubmit() {
 }
   
 function toggleCheckedForListItem(itemIndex) {
-  console.log('Toggling checked property for item at index ' + itemIndex);
   STORE[itemIndex].checked = !STORE[itemIndex].checked;
 }
   
@@ -73,7 +67,6 @@ function getItemIndexFromElement(item) {
   
 function handleItemCheckClicked() {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
-    console.log('`handleItemCheckClicked` ran');
     const itemIndex = getItemIndexFromElement(event.currentTarget);
     toggleCheckedForListItem(itemIndex);
     renderShoppingList();
@@ -92,27 +85,26 @@ function handleDeleteItemClicked() {
     deleteItemFromList(itemIndex);
     renderShoppingList();
   });
-  console.log('`handleDeleteItemClicked` ran');
 }
 
 function toggleEditItem(itemIndex) {
   STORE[itemIndex].editable = !STORE[itemIndex].editable;
 }
 
-function toggleEditButton(itemIndex){
-//when editable is TRUE; button = save
-  const editButton = `
-    <button class="shopping-item-edit js-item-edit">
-      <span class="button-label">edit</span>
-    </button>`; //js-item-edit;
-  const saveButton = `
-    <button class="shopping-item-save js-item-save">
-      <span class="button-label">save</span>
-    </button>`; //js-item-save;
-  //STORE[itemIndex].editable ? 
-  $('.js-item-edit').replaceWith(saveButton);
-  //: $('.js-item-edit').replaceWith(editButton);
-}
+// function toggleEditButton(itemIndex){
+// //when editable is TRUE; button = save
+//   const editButton = `
+//     <button class="shopping-item-edit js-item-edit">
+//       <span class="button-label">edit</span>
+//     </button>`; //js-item-edit;
+//   const saveButton = `
+//     <button class="shopping-item-save js-item-save">
+//       <span class="button-label">save</span>
+//     </button>`; //js-item-save;
+//   //STORE[itemIndex].editable ? 
+//   $('.js-item-edit').replaceWith(saveButton);
+//   //: $('.js-item-edit').replaceWith(editButton);
+// }
 
 
 // User can edit an item title
@@ -121,24 +113,24 @@ function handleEditItemClicked(){
   $('.js-shopping-list').on('click', '.js-item-edit', (event) => {
     const itemIndex = getItemIndexFromElement(event.currentTarget);
     toggleEditItem(itemIndex);
-    toggleEditButton(itemIndex);
+    //toggleEditButton(itemIndex);
     renderShoppingList();
   });
 }
 
-function handleSaveItemClicked(){
-  //Listen for new input when user click on Save Button
+// function handleSaveItemClicked(){
+//   //Listen for new input when user click on Save Button
+// }
+
+function toggleHideButton(){
+  //Toggle completed items to hide;
+  $('.js-shopping-list').find('.shopping-item__checked').parent().toggle('hide_checked');
 }
 
-function toggleFinishButton(){
-  //Toggle item from itemIndex of STORE to hide;
-  $('.js-shopping-list').find('.shopping-item__checked').parent().hide();
-}
-
-function handleFinishedItemClicked(){
+function handleHideItemClicked(){
 // User can press a toggle switch to show all items or show only items that are unchecked
   $('.js-list-finish').on('click', (event) => {
-    toggleFinishButton();
+    toggleHideButton();
   });
 }
   
@@ -161,7 +153,7 @@ function handleShoppingList() {
   handleItemCheckClicked();
   handleDeleteItemClicked();
   handleEditItemClicked();
-  handleFinishedItemClicked();
+  handleHideItemClicked();
 }
   
 $(handleShoppingList);
